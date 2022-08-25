90d728cbe564353823b52e5245d3bb6c1d4dfa65
Carlos Ospina <cardaos2204@gmail.com>
2022-08-24 20:54:51 -0500

    Commit
    24082022

---
+++ src/controllers/controllerCinema.js
@@ -0,0 +1,106 @@
+let peliculas = [
+    {
+        Nombre: "Bestia",
+        Genero: "Acción",
+        Duracion: "120 min",
+        Clasificacion: "B",
+        Sinopsis: "A veces el susurro en los arbustos es en realidad un monstruo. Idris Elba (Rápidos y Furiosos: Hobbs & Shaw, El Escuadrón Suicida) protagoniza un nuevo y emocionante thriller sobre un padre y sus dos hijas adolescentes que se ven perseguidos por un enorme león que intenta demostrar que la selva sólo tiene un máximo depredador. Elba interpreta al Dr. Nate Daniels, un marido que acaba de enviudar y que regresa a Sudáfrica, donde conoció a su mujer, en un viaje que llevaba tiempo planeando con sus hijas a una zona de caza controlada por Martin Battles (Sharlto Copley, serie Muñeca Rusa, Maléfica), un viejo amigo de la familia y biólogo especializado en vida salvaje. Pero lo que comienza como un viaje de sanación se convierte en una aterradora lucha por sobrevivir cuando un león, que ha escapado de los cazadores clandestinos sediento de sangre y que ahora ve a todos los humanos como el enemigo, comienza a acecharlos. Iyana Halley (The Hate U Give, serie This is Us) interpreta a la hija de Daniels, Meredith, de 18 años, y Leah Sava Jeffries (serie Rel, serie Empire) a su hija de 13 años, Norah. De Baltasar Kormákur, director de Everesty de las películas de Universal Pictures 2 Guns y Contrabando, Bestia es producida por Will Packer, el exitoso productor de Plan de chicas, la franquicia Un novato en apuros y diez películas que han abierto el número 1 en la taquilla de Estados Unidos, incluyendo Escuela Nocturna, Favor peligroso y Piensa como hombre, de James Lopez, presidente de Will Packer Productions, y de Baltasar Kormákur. La película está escrita por Ryan Engle (Rampage, Non-Stop) a partir de una historia original de Jaime Primak Sullivan y cuenta con la producción ejecutiva de Jaime Primak Sullivan y Bernard Bellew.",
+        Imagen: "https://firebasestorage.googleapis.com/v0/b/fotoscartelera-f621d.appspot.com/o/Bestia.jpg?alt=media&token=c855307a-9f23-451c-8ede-7daa2010e02a",
+        Idioma: "Español"
+    },
+    {
+        Nombre: "Buena Suerte Leo Grande",
+        Genero: "Comedia",
+        Duracion: "120 min",
+        Clasificacion: "B",
+        Sinopsis: "Nancy Stokes (Emma Thompson) es una mujer viuda que nunca se permitió sentir placer, hasta ahora. En una habitación de hotel decide contratar a un atractivo joven trabajador sexual, llamado Leo Grande (Daryl McCormack), con el deseo de probar y disfrutar de una noche de placer y autodescubrimiento. Pero ninguno de los dos imaginó descubrir tanto sobre sí mismos. Una historia divertida y sensible sobre el deseo, la sexualidad y las conexiones humanas.",
+        Imagen: "https://firebasestorage.googleapis.com/v0/b/fotoscartelera-f621d.appspot.com/o/BuenaSuerteLeoGrande.png?alt=media&token=f5eb82ab-483e-415b-9022-2ff453603b7e",
+        Idioma: "Español"
+    },
+    {
+        Nombre: "Dragon Ball Super Heroes",
+        Genero: "Animada",
+        Duracion: "100 min",
+        Clasificacion: "A",
+        Sinopsis: "La malvada organización Red Ribbon Army se reforma con nuevos y más poderosos androides, Gamma 1 y Gamma 2 para buscar venganza.",
+        Imagen: "https://firebasestorage.googleapis.com/v0/b/fotoscartelera-f621d.appspot.com/o/DragonBallSuperHero.jpg?alt=media&token=9ec5868e-428d-4ddf-8aa8-30a5d37d3a65",
+        Idioma: "Japones"
+    },
+    {
+        Nombre: "El Fantasma de Martys Lane",
+        Genero: "Terror",
+        Duracion: "140 min",
+        Clasificacion: "B",
+        Sinopsis: "Leah, una niña de 10 años que vive en una vieja casa que guarda tantos secretos como su propia familia, empieza a recibir las visitas nocturnas de un hombre inquietante que conoce las respuestas que ella tanto ansía.",
+        Imagen: "https://firebasestorage.googleapis.com/v0/b/fotoscartelera-f621d.appspot.com/o/ElFantasmaDeMartyrsLane.jpg?alt=media&token=62274ca6-fa6b-40e8-9ae4-0b9545b05b12",
+        Idioma: "Inglés"
+    },
+    {
+        Nombre: "El Leopardo De Las Nieves",
+        Genero: "Animada",
+        Duracion: "100 min",
+        Clasificacion: "B",
+        Sinopsis: "Las altas mesetas del Tibet. En el corazón de los valles vírgenes e inaccesibles se encuentra uno de los últimos santuarios de la vida salvaje, habitada por una fauna desconocida y poco común. Vincent Munier, uno de los fotógrafos de naturaleza más famosos del mundo, viaja junto al aventurero y escritor Sylvain Tesson, por quien siente una gran admiración. Durante varias semanas, van a recorrer estos valles en busca de animales singulares y tratar de avistar el leopardo de las nieves, uno de los felinos más raros y más difíciles de abordar. Esta búsqueda de lo invisible será una revelación para Sylvain Tesson. Con la ayuda de Vincent Munier, va a aprender a descifrar estos lugares salvajes, volver a descubrir la espera, el silencio y la observación. Un regreso a la esencia animal del hombre, la humildad frente a la naturaleza que será la clave para que ésta nazca finalmente en su interior.",
+        Imagen: "https://firebasestorage.googleapis.com/v0/b/fotoscartelera-f621d.appspot.com/o/ElLeopardoDeLasNieves.png?alt=media&token=cda91c7b-4a1e-4c44-abe2-ca5c7cd5e745",
+        Idioma: "Español"
+    },
+    {
+        Nombre: "Gemelo Siniestro",
+        Genero: "Acción",
+        Duracion: "120 min",
+        Clasificacion: "B",
+        Sinopsis: "Tras la muerte de uno de sus hijos gemelos, Rachel y Anthony se instalan en el campo escandinavo para sanar las heridas anímicas. Allí, sin embargo, ella descubre que una fuerza sobrenatural y maligna quiere llevarse al pequeño que le queda.",
+        Imagen: "https://firebasestorage.googleapis.com/v0/b/fotoscartelera-f621d.appspot.com/o/GemeloSiniestro.png?alt=media&token=52cb2ee0-1801-4781-beae-568fa5fee558",
+        Idioma: "Alemán"
+    },
+    {
+        Nombre: "Jack Caja Maldita",
+        Genero: "Acción",
+        Duracion: "120 min",
+        Clasificacion: "B",
+        Sinopsis: "Cuando una mujer moribunda abre una caja antigua, hace un trato con el demonio. Éste curará su enfermedad a cambio de ayudarla a reclamar seis víctimas inocentes.",
+        Imagen: "https://firebasestorage.googleapis.com/v0/b/fotoscartelera-f621d.appspot.com/o/JackCajaMaldita2.png?alt=media&token=391a6f11-9c6c-4546-adef-29c797f8ed5e",
+        Idioma: "Español"
+    },
+    {
+        Nombre: "Minions Nace Un Villano",
+        Genero: "Animada",
+        Duracion: "100 min",
+        Clasificacion: "B",
+        Sinopsis: "En los años 70, Gru crece siendo un gran admirador de 'Los salvajes seis', un supergrupo de villanos. Para demostrarles que puede ser malvado, Gru idea un plan con la esperanza de formar parte de la banda. Por suerte, cuenta con la ayuda de sus fieles seguidores, los Minions, siempre dispuestos a sembrar el caos.",
+        Imagen: "https://firebasestorage.googleapis.com/v0/b/fotoscartelera-f621d.appspot.com/o/MinionNaceUnVillano.jpg?alt=media&token=a6f6882b-0df8-4d0e-b219-3686e4ee24f9",
+        Idioma: "Inglés"
+    },
+    {
+        Nombre: "Sana que sana",
+        Genero: "Animada",
+        Duracion: "100 min",
+        Clasificacion: "B",
+        Sinopsis: "Rogelio Pataquiva como “todero” de la Clínica Verde se ve enfrentado a una gran disyuntiva: apoyar a los “súpercalvos”, -niños con cáncer- lo que le significaría aplazar su sueño de conocer el mar o seguir con su vida “normal” y aburrida.",
+        Imagen: "https://firebasestorage.googleapis.com/v0/b/fotoscartelera-f621d.appspot.com/o/SanaQueSana.png?alt=media&token=949e5707-bdb6-4495-94fd-c7aa497589c2",
+        Idioma: "Ruso"
+    },
+    {
+        Nombre: "Super Mascotas",
+        Genero: "Animada",
+        Duracion: "100 min",
+        Clasificacion: "B",
+        Sinopsis: "El superperro Krypto une a un grupo de divertidos y descerebrados animales para que lo ayuden a rescatar a los superhéroes de la Liga de la Justicia, que han sido secuestrados.",
+        Imagen: "https://firebasestorage.googleapis.com/v0/b/fotoscartelera-f621d.appspot.com/o/SuperMascotas.jpg?alt=media&token=3d2026db-da92-4778-87d3-76d2fe53c1f4",
+        Idioma: "Español"
+    }
+]
+
+// Recorriendo un arreglo en JavaScript
+
+// Metodos del array
+
+peliculas.forEach(function (pelicula) {
+    console.log(pelicula.Nombre);
+    console.log(pelicula.Genero);
+    console.log(pelicula.Duracion);
+    console.log(pelicula.Clasificacion);
+    console.log(pelicula.Sinopsis);
+    console.log(pelicula.Imagen);
+    console.log(pelicula.Idioma);
+})
